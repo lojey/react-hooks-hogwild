fdab786c7fc7847132579dd7c4772a8a2e0de7cf
Gloria Jeyech <gjeyech@gmail.com>
2024-09-13 09:52:37 +0300

    app

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React , {useState} from "react";
 import Nav from "./Nav";
+import HogList from "./HogList";
+import Filter from "./Filter";
+import HogForm from "./HogForm";
 
 import hogs from "../porkers_data";
 
 function App() {
+	const [hogsData, setHogsData] = useState(hogs);
+
 	return (
 		<div className="App">
 			<Nav />
+			<Filter setHogs={setHogsData} hogs={hogsData} />
+      <HogList hogs={hogsData} />
+      <HogForm setHogs={setHogsData} />	
 		</div>
 	);
 }
